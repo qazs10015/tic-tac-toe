7f99ba0852c65f31edadb3ef9bf47338007a0007
JunyuWang <happystyle666@gmail.com>
2023-09-07 22:02:01 +0800

    modifiy next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: '/tic-tac-toe',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
